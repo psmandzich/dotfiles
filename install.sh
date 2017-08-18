5cf4258834765f4ba4f0fc2695ba9b8dd87a7468
psmandzich <psmandzich@users.noreply.github.com>
2017-08-18 11:15:31 +0200

    added zsh

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 DOT_URI='https://raw.githubusercontent.com/psmandzich/dotfiles/master/'
 
+# VIM
 curl 'http://vim-bootstrap.com/generate.vim' --data 'langs=javascript&langs=php&langs=html&langs=ruby&editor=vim' > ~/.vimrc
 curl "${DOT_URI}vimrc.local" > ~/.vimrc.local
+
+# ZSH -- oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+curl "${DOT_URI}zshrc" > ~/.zshrc
