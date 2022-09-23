8adb5fad2e31960322e94b723dfb7f563a46a42c
Patrick Smandzich <patrick.smandzich@42he.com>
2022-09-23 12:12:21 +0200

    add fonts to remove container

--- vs-code-remote-install.sh
+++ vs-code-remote-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo apt update && sudo apt install -y --no-install-recommends openssh-client zsh vim wget
+sudo apt update
+sudo apt install -y --no-install-recommends openssh-client zsh vim wget fonts-powerline ttf-ancient-fonts
 
 ./install.sh
