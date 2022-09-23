36063662bf321c55fcff77057c408bc041158b21
Patrick Smandzich <patrick.smandzich@42he.com>
2022-09-23 13:39:11 +0200

    ensure correct setting of locale

--- vs-code-remote-install.sh
+++ vs-code-remote-install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y --no-install-recommends openssh-client zsh vim wget fonts-powerline ttf-ancient-fonts
+sudo apt install -y --no-install-recommends openssh-client zsh vim wget fonts-powerline ttf-ancient-fonts locales
+sudo sh -c 'echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && dpkg-reconfigure --frontend=noninteractive locales'
 
 ./install.sh
